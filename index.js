d1dc22bf0493542a56401c8b522320f829c2b8fd
Elvis Garramone <Egarramone@gmail.com>
2022-07-29 14:09:22 -1000

    update to the code

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ function moveBackground(event) {
     for (let i = 0; i < shapes.length; i++) {
         const isOdd = i % 2 !== 0;
         const boolInt = isOdd ? -1 : 1;
-        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
     }
 }
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
     if(contrastToggle) {
-        document.body.classList += " dark-theme";
+        document.body.classList += " dark-theme"
     }
     else {
-        document.body.classList.remove("dark-theme");
+        document.body.classList.remove("dark-theme")
     }
 }
 
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible"
-
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
     emailjs
     .sendForm(
         'service_7wqc8j7',
@@ -55,21 +54,5 @@ function toggleModal() {
     isModalOpen = true;
     document.body.classList += " modal--open";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
